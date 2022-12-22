fecd016098f4f79293f5b363b93ed996588de485
TangeRina1110 <115104678+TangeRina1110@users.noreply.github.com>
2022-12-22 22:36:31 +0300

    Add files via upload

---
+++ Homework3/oboloch.java
@@ -0,0 +1,20 @@
+package Homework3;
+
+public class oboloch {
+    public static void main(String[] args) {
+        //1
+        Double db = Double.valueOf(2.4);
+        //2
+        String str = "2.5";
+        Double dbr = Double.parseDouble(str);
+        //3
+        int i = db.intValue();
+        float f = db.floatValue();
+        byte b = db.byteValue();
+        short s = db.shortValue();
+        //4
+        System.out.println(db+ " " + i);
+        //5
+        String str1 = Double.toString(db);
+    }
+}

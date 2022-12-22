66743cb62a270ab117e9233f28ef3ecaeafdfcd7
TangeRina1110 <115104678+TangeRina1110@users.noreply.github.com>
2022-12-22 22:26:15 +0300

    Add files via upload

---
+++ Homework25/IPAddress.java
@@ -0,0 +1,22 @@
+package Homework25;
+
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IPAddress {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(true){
+            String ipAddressPattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+            Pattern pattern = Pattern.compile(ipAddressPattern);
+            System.out.print("Enter IP: ");
+            Matcher matcher = pattern.matcher(in.nextLine());
+            System.out.println(matcher.matches());
+        }
+    }
+}
